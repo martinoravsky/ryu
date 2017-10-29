606b20c67e6b886a9aa0f29f9c65d9e0bc731c89
martinoravsky <c8dQIEBuT>
2017-10-29 11:39:32 +0100

    PyCharm files

--- ryu/app/shortest_path_forwarding_13.py
+++ ryu/app/shortest_path_forwarding_13.py
@@ -16,29 +16,27 @@ from ryu.lib.packet import packet, ethernet, ether_types
 
 from ryu.topology.api import get_switch, get_link
 from ryu.app.wsgi import ControllerBase
-from ryu.topology import event, switches 
+from ryu.topology import event, switches
 import networkx as nx
 
+
 class ProjectController(app_manager.RyuApp):
-	
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
 
     def __init__(self, *args, **kwargs):
         super(ProjectController, self).__init__(*args, **kwargs)
         self.mac_to_port = {}
         self.topology_api_app = self
-        self.net=nx.DiGraph()
+        self.net = nx.DiGraph()
         self.nodes = {}
         self.links = {}
         self.no_of_nodes = 0
         self.no_of_links = 0
-        self.i=0
-
+        self.i = 0
 
     # Handy function that lists all attributes in the given object
-    def ls(self,obj):
+    def ls(self, obj):
         print("\n".join([x for x in dir(obj) if x[0] != "_"]))
-
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
@@ -58,7 +56,6 @@ class ProjectController(app_manager.RyuApp):
                                           ofproto.OFPCML_NO_BUFFER)]
         self.add_flow(datapath, 0, match, actions)
 
-
     def add_flow(self, datapath, priority, match, actions, buffer_id=None):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
@@ -73,7 +70,6 @@ class ProjectController(app_manager.RyuApp):
             mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
                                     match=match, instructions=inst)
         datapath.send_msg(mod)
-   
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev):
@@ -101,24 +97,24 @@ class ProjectController(app_manager.RyuApp):
         self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
 
         # learn a mac address to avoid FLOOD next time.
-        self.mac_to_port[dpid][src] = in_port 
+        self.mac_to_port[dpid][src] = in_port
 
         for f in msg.match.fields:
             if f.header == ofproto_v1_3.OXM_OF_IN_PORT:
                 in_port = f.value
-        print 'Som pred ifom' 
+        print('Som pred ifom')
         if src not in self.net:
-            print 'Toto by sa malo vypisat iba raz'
+            print('Toto by sa malo vypisat iba raz')
             self.net.add_node(src)
-            self.net.add_edge(dpid,src,port=in_port)
-            self.net.add_edge(src,dpid)
+            self.net.add_edge(dpid, src, port=in_port)
+            self.net.add_edge(src, dpid)
         if dst in self.net:
-            print (src in self.net)
-            print nx.shortest_path(self.net,src,dst)
+            print(src in self.net)
+            print(nx.shortest_path(self.net, src, dst))
 
-            path=nx.shortest_path(self.net,src,dst)   
-            next=path[path.index(dpid)+1]
-            out_port=self.net[dpid][next]['port']
+            path = nx.shortest_path(self.net, src, dst)
+            next = path[path.index(dpid) + 1]
+            out_port = self.net[dpid][next]['port']
         else:
             out_port = ofproto.OFPP_FLOOD
 
@@ -140,29 +136,30 @@ class ProjectController(app_manager.RyuApp):
 
         out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
                                   in_port=in_port, actions=actions, data=data)
-        datapath.send_msg(out) 
+        datapath.send_msg(out)
+
     @set_ev_cls(event.EventSwitchEnter)
     def get_topology_data(self, ev):
-        switch_list = get_switch(self.topology_api_app, None)   
-        switches=[switch.dp.id for switch in switch_list]
+        switch_list = get_switch(self.topology_api_app, None)
+        switches = [switch.dp.id for switch in switch_list]
         self.net.add_nodes_from(switches)
-         
-        #print "**********List of switches"
-        #for switch in switch_list:
-        #self.ls(switch)
-        #print switch
-        #self.nodes[self.no_of_nodes] = switch
-        #self.no_of_nodes += 1
+
+        # print "**********List of switches"
+        # for switch in switch_list:
+        # self.ls(switch)
+        # print switch
+        # self.nodes[self.no_of_nodes] = switch
+        # self.no_of_nodes += 1
 
         links_list = get_link(self.topology_api_app, None)
-        #print links_list
-        links=[(link.src.dpid,link.dst.dpid,{'port':link.src.port_no}) for link in links_list]
-        #print links
+        # print links_list
+        links = [(link.src.dpid, link.dst.dpid, {'port': link.src.port_no}) for link in links_list]
+        # print links
         self.net.add_edges_from(links)
-        links=[(link.dst.dpid,link.src.dpid,{'port':link.dst.port_no}) for link in links_list]
-        #print links
+        links = [(link.dst.dpid, link.src.dpid, {'port': link.dst.port_no}) for link in links_list]
+        # print links
         self.net.add_edges_from(links)
-        print "**********List of links"
-        print self.net.edges()
-        print links
-        print switches
+        print("**********List of links")
+        print(self.net.edges())
+        print(links)
+        print(switches)
