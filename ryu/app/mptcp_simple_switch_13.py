8697fcbe0dd389ec58f305aa179803b957d779b8
Martin Oravsky <xoravskym@stuba.sk>
2017-10-30 07:51:28 -0700

    pomoc

--- ryu/app/mptcp_simple_switch_13.py
+++ ryu/app/mptcp_simple_switch_13.py
@@ -4,7 +4,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#    http://www.apache.org/licenses/LICENSE-2.0
+#	 http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -30,134 +30,147 @@ import networkx as nx
 import binascii
 
 class L2switch(app_manager.RyuApp):
-    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+	OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
 
-    def __init__(self, *args, **kwargs):
-        super(L2switch, self).__init__(*args, **kwargs)
-        self.mac_to_port = {}
+	def __init__(self, *args, **kwargs):
+		super(L2switch, self).__init__(*args, **kwargs)
+		self.mac_to_port = {}
 
-    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
-    def switch_features_handler(self, ev):
-        datapath = ev.msg.datapath
-        ofproto = datapath.ofproto
-        parser = datapath.ofproto_parser
+	@set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+	def switch_features_handler(self, ev):
+		datapath = ev.msg.datapath
+		ofproto = datapath.ofproto
+		parser = datapath.ofproto_parser
 
-        # install table-miss flow entry
-        #
-        # We specify NO BUFFER to max_len of the output action due to
-        # OVS bug. At this moment, if we specify a lesser number, e.g.,
-        # 128, OVS will send Packet-In with invalid buffer_id and
-        # truncated packet data. In that case, we cannot output packets
-        # correctly.  The bug has been fixed in OVS v2.1.0.
-        match = parser.OFPMatch()
-        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
-                                          ofproto.OFPCML_NO_BUFFER)]
-        self.add_flow(datapath, 0, match, actions)
+		# install table-miss flow entry
+		#
+		# We specify NO BUFFER to max_len of the output action due to
+		# OVS bug. At this moment, if we specify a lesser number, e.g.,
+		# 128, OVS will send Packet-In with invalid buffer_id and
+		# truncated packet data. In that case, we cannot output packets
+		# correctly.  The bug has been fixed in OVS v2.1.0.
+		match = parser.OFPMatch()
+		actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
+										  ofproto.OFPCML_NO_BUFFER)]
+		self.add_flow(datapath, 0, match, actions)
 
-    def add_flow(self, datapath, priority, match, actions, buffer_id=None):
-        ofproto = datapath.ofproto
-        parser = datapath.ofproto_parser
+	def add_flow(self, datapath, priority, match, actions, buffer_id=None):
+		ofproto = datapath.ofproto
+		parser = datapath.ofproto_parser
 
-        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
-                                             actions)]
-        if buffer_id:
-            mod = parser.OFPFlowMod(datapath=datapath, buffer_id=buffer_id,
-                                    priority=priority, match=match,
-                                    instructions=inst)
-        else:
-            mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
-                                    match=match, instructions=inst)
-        datapath.send_msg(mod)
+		inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
+											 actions)]
+		if buffer_id:
+			mod = parser.OFPFlowMod(datapath=datapath, buffer_id=buffer_id,
+									priority=priority, match=match,
+									instructions=inst)
+		else:
+			mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
+									match=match, instructions=inst)
+		datapath.send_msg(mod)
 
-    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
-    def _packet_in_handler(self, ev):
-        # If you hit this you might want to increase
-        # the "miss_send_length" of your switch
-        if ev.msg.msg_len < ev.msg.total_len:
-            self.logger.debug("packet truncated: only %s of %s bytes",
-                              ev.msg.msg_len, ev.msg.total_len)
-        msg = ev.msg
-        datapath = msg.datapath
-        ofproto = datapath.ofproto
-        parser = datapath.ofproto_parser
-        in_port = msg.match['in_port']
+	@set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+	def _packet_in_handler(self, ev):
+		# If you hit this you might want to increase
+		# the "miss_send_length" of your switch
+		if ev.msg.msg_len < ev.msg.total_len:
+			self.logger.debug("packet truncated: only %s of %s bytes",
+							  ev.msg.msg_len, ev.msg.total_len)
+		msg = ev.msg
+		datapath = msg.datapath
+		ofproto = datapath.ofproto
+		parser = datapath.ofproto_parser
+		in_port = msg.match['in_port']
 
-        pkt = packet.Packet(msg.data)
-        eth = pkt.get_protocols(ethernet.ethernet)[0]
+		pkt = packet.Packet(msg.data)
+		eth = pkt.get_protocols(ethernet.ethernet)[0]
 
-        if eth.ethertype == ether_types.ETH_TYPE_LLDP:
-            # ignore lldp packet
-            return
+		if eth.ethertype == ether_types.ETH_TYPE_LLDP:
+			# ignore lldp packet
+			return
 
-        dst = eth.dst
-        src = eth.src
+		dst = eth.dst
+		src = eth.src
 
 		#self.logger.info("taketo protokoly su:")
 
-        t = pkt.get_protocol(ipv4.ipv4)
+		t = pkt.get_protocol(ipv4.ipv4)
 
-        if t:
-            print 'zdrojova ip: ',t.src
-            print 'dest ip: ',t.dst
+		if t:
+			print 'zdrojova ip: ',t.src
+			print 'dest ip: ',t.dst
 
-        ht = pkt.get_protocol(tcp.tcp)
+		ht = pkt.get_protocol(tcp.tcp)
 
-        if ht:
-            print 'zdrojovy port: ',ht.src_port
-            print 'destination port: ',ht.dst_port
+		if ht:
+			print 'zdrojovy port: ',ht.src_port
+			print 'destination port: ',ht.dst_port
 
-            options = ht.option
+			options = ht.option
 
-            join = 0
-            if options:
-                if len(options) > 0:
-                    for opt in options:
-                        print opt
-                        if opt.kind == 30:
-                            print(int(binascii.hexlify(opt.value)[4:],16))
+			if options:
+				if len(options) > 0:
+					for opt in options:
+						print opt
+						if opt.kind == 30: # MPTCP
+							hexopt = binascii.hexlify(opt.value)
+							if hexopt[:2] == "00":          # MP_CAPABLE
+                                if ht.bits == 2:            # SYN
+									print("MP_CAPABLE SYN. Sender's key: ", int(hexopt[4:],16))
+								elif ht.bits == 12:         # SYN-ACK
+									print("MP_CAPABLE SYN-ACK. Receivers'key: ", int(hexopt[4:],16))
+								elif ht.bits == 10:         # ACK
+									print("MP_CAPABLE ACK. Already have keys.")
+							elif hexopt[:2] == "10":        # MP_JOIN
+								if ht.bits == 2:            # SYN
+									print("MP_JOIN SYN. Sender's nonce: ", int(hexopt[4:],16))
+								elif ht.bits == 12:         # SYN-ACK
+									print("MP_JOIN SYN-ACK. Sender's truncated HMAC :")
+								elif ht.bits == 10:         # ACK
+									print("MP_JOIN ACK. Sender's HMAC :", int(hexopt[4:],16))
 
-            if ht.src_port == 80:
-                print
-                'HTTP!!!'
-            elif ht.dst_port == 80:
-                print
-                'HTTP!!!'
+			if ht.src_port == 80:
+				print
+				'HTTP!!!'
+			elif ht.dst_port == 80:
+				print
+				'HTTP!!!'
 
-        dpid = datapath.id
-        self.mac_to_port.setdefault(dpid, {})
+		dpid = datapath.id
+		self.mac_to_port.setdefault(dpid, {})
 
-        self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
+		self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
 
-        # learn a mac address to avoid FLOOD next time.
-        self.mac_to_port[dpid][src] = in_port
+		# learn a mac address to avoid FLOOD next time.
+		self.mac_to_port[dpid][src] = in_port
 
-        if dst in self.mac_to_port[dpid]:
-            out_port = self.mac_to_port[dpid][dst]
-        else:
-            out_port = ofproto.OFPP_FLOOD
+		if dst in self.mac_to_port[dpid]:
+			out_port = self.mac_to_port[dpid][dst]
+		else:
+			out_port = ofproto.OFPP_FLOOD
 
-        actions = [parser.OFPActionOutput(out_port)]
+		actions = [parser.OFPActionOutput(out_port)]
 
-        # install a flow to avoid packet_in next time
-        if out_port != ofproto.OFPP_FLOOD:
-            match = parser.OFPMatch(in_port=in_port, eth_dst=dst)
-            # verify if we have a valid buffer_id, if yes avoid to send both
-            # flow_mod & packet_out
-            if msg.buffer_id != ofproto.OFP_NO_BUFFER:
-                self.add_flow(datapath, 1, match, actions, msg.buffer_id)
-                return
-            else:
-                self.add_flow(datapath, 1, match, actions)
-        data = None
-        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
-            data = msg.data
+		# install a flow to avoid packet_in next time
+		if out_port != ofproto.OFPP_FLOOD:
+			match = parser.OFPMatch(in_port=in_port, eth_dst=dst)
+			# verify if we have a valid buffer_id, if yes avoid to send both
+			# flow_mod & packet_out
+			if msg.buffer_id != ofproto.OFP_NO_BUFFER:
+				self.add_flow(datapath, 1, match, actions, msg.buffer_id)
+				return
+			else:
+				self.add_flow(datapath, 1, match, actions)
+		data = None
+		if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+			data = msg.data
 
-        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
-                                  in_port=in_port, actions=actions, data=data)
-        datapath.send_msg(out)
+		out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+								  in_port=in_port, actions=actions, data=data)
+		datapath.send_msg(out)
 
-    # @set_ev_cls(event.EventSwitchEnter)
-    # def get_topology_data(self, ev):
+	# @set_ev_cls(event.EventSwitchEnter)
+	# def get_topology_data(self, ev):
 		# switch_list = get_switch(self.topology_api_app, None)
 		# switches=[switch.dp.id for switch in switch_list]
 		# links_list = get_link(self.topology_api_app, None)
