1d78eb3e7fd6e7de361bc8c32706c5ae9e2386bf
martinoravsky <martinoravskyjr@gmail.com>
2018-03-24 08:20:28 +0100

    novy stroj

--- kod/connection.py
+++ kod/connection.py
@@ -1,7 +1,7 @@
 import mysql
 
 def connect():
-    conn = mysql.connector.connect(host='192.168.56.105',
+    conn = mysql.connector.connect(host='192.168.56.101',
 								    database='mptcp',
 									user='mptcp',
 									password='mptcp123')
